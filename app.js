f1c1c85b2d161aaedd05600e3a2ce518f61c4571
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-11-05 19:45:15 -0400

    Added link to form. Commented code.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+// Defines the app as an angular module.
 var app = angular.module('Newsly', []);
 
+// Main conterller referenced in the <body> tag.
 app.controller('MainCtrl', [
   '$scope',
   function($scope){
@@ -12,13 +14,20 @@ app.controller('MainCtrl', [
       {title: 'post 4', upvotes: 9},
       {title: 'post 5', upvotes: 4}
     ];
-
+// addPost function
     $scope.addPost = function(){
+      // Stops a user from submitting a blank title
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      // Pushes the new post to the $scope.post array
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
+      $scope.link = '';
     };
-
+// incrementUpvotes function
     $scope.incrementUpvotes = function(post){
       post.upvotes += 1;
     };
