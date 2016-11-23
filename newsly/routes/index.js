20b1d46be150741191f6792925193ac7af863dab
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-11-23 18:48:35 -0500

    Added route for adding upvotes.

--- newsly/routes/index.js
+++ newsly/routes/index.js
@@ -46,6 +46,14 @@ router.get('/posts/:post', function(req, res) {
   res.json(req.post);
 });
 
+// route for adding upvotes
+router.put('/posts/:post/upvote', function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
 
 
 module.exports = router;
