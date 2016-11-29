33872de98ea9feedf0be7e9479b2a8201b661103
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-11-28 19:58:36 -0500

    POST route still broken.

--- newsly/routes/index.js
+++ newsly/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' } );
 });
 
 var mongoose = require('mongoose');
@@ -13,10 +13,10 @@ var Comment = mongoose.model('Comment');
 // GET Request
 router.get('/posts', function(req, res, next){
   Post.find(function(err, posts){
-    if(err) { return next(err); }
+    if(err) { next(err); }
 
     res.json(posts);
-  });
+  })
 });
 
 router.post('/posts', function(req, res, next) {
@@ -32,7 +32,6 @@ router.post('/posts', function(req, res, next) {
 // route for automatically preloading post objects ****uses Expressjs' param() function****
 router.param('post', function(req,res,next,id) {
   var query = Post.findById(id);
-
   // Use's mongoose's query interface which provides a more flexible way of interacting with the database.
   query.exec(function (err, post) {
     if (err) { return next(err); }
@@ -48,8 +47,6 @@ router.get('/posts/:post', function(req, res) {
   res.json(req.post);
 });
 
-module.exports = router;
-
 // route for adding upvotes
 router.put('/posts/:post/upvote', function(req, res, next) {
   req.post.upvote(function(err, post){
@@ -58,3 +55,5 @@ router.put('/posts/:post/upvote', function(req, res, next) {
     res.json(post);
   });
 });
+
+module.exports = router;
