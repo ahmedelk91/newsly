a618e6121bc3fe0c2ba26cb19806b1fffd772a67
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2017-01-29 16:03:40 -0500

    Added user model.

---
+++ newsly/models/Users.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+// used to has passwords
+var crypto = require('cryto');
+var jwt = require('jsonwebtoken');
+
+var UserSchema = new mongoose.Schema({
+  username: {type: String, lowercase: true, unique: true},
+  // Stores the hash of the password
+  hash: String,
+  salt: String
+});
+
+UserSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+// method that accepts the password and compares it to the hash stored, returning a boolean
+UserSchema.methods.validPassword = function(password){
+var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex')
+
+return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+
+// sets expiration to 60 days
+  var today = new Date();
+  var exp = new Date(today);
+  exp.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(exp.getTime() / 1000),
+  }, 'SECRET')
+};
+
+mongoose.model('User', UserSchema);
