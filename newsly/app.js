427be532a16de9b257dcbdb64710ea775ec1cb04
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2017-01-29 16:42:23 -0500

    Set up passport.

--- newsly/app.js
+++ newsly/app.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
+var passport = require('passport');
 require('./models/Posts');
 require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
 mongoose.connect('mongodb://localhost/news');
 
 
@@ -27,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', index);
 // app.use('/users', users);
